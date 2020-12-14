45e3884692c6735b1b6e79c55a4b6bd06ec191a7
rhkdhl23 <rhkdhl23@naver.com>
2020-12-15 00:34:43 +0900

    firebase 연동 _ 로그인 및 회원가입 구현

---
+++ app/src/main/java/com/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.activity.healthdiet_01.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
